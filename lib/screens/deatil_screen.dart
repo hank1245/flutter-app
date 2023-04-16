ea8b3ec2710f82c919dc244052f74fb72ae4bf0d
Hank Kim <hank29206880@gmail.com>
2023-04-16 17:24:25 +0900

    hero animation

--- lib/screens/deatil_screen.dart
+++ lib/screens/deatil_screen.dart
@@ -21,9 +21,34 @@ class DetailScreen extends StatelessWidget {
           ),
         ),
         body: Column(
-          children: const [
-            SizedBox(
+          children: [
+            const SizedBox(
               height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Hero(
+                  tag: id,
+                  child: Container(
+                    width: 250,
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 15,
+                          offset: const Offset(10, 10),
+                          color: Colors.black.withOpacity(0.5),
+                        ),
+                      ],
+                    ),
+                    child: Image.network(
+                      thumb,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ));
