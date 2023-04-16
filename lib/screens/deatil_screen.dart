3bbf0e896de451fa024def3edb8b6415d5a2d539
Hank Kim <hank29206880@gmail.com>
2023-04-16 19:22:15 +0900

    stateful widget

--- lib/screens/deatil_screen.dart
+++ lib/screens/deatil_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/models/webtoon_detail_model.dart';
+import 'package:myapp/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
+
   const DetailScreen(
       {super.key, required this.title, required this.thumb, required this.id});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +29,7 @@ class DetailScreen extends StatelessWidget {
           elevation: 0,
           backgroundColor: Colors.green,
           title: Text(
-            title,
+            widget.title,
             style: const TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.w600,
@@ -29,7 +45,7 @@ class DetailScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Hero(
-                  tag: id,
+                  tag: widget.id,
                   child: Container(
                     width: 250,
                     clipBehavior: Clip.hardEdge,
@@ -44,7 +60,7 @@ class DetailScreen extends StatelessWidget {
                       ],
                     ),
                     child: Image.network(
-                      thumb,
+                      widget.thumb,
                     ),
                   ),
                 ),
